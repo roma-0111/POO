ef01e7eae47fa3a0cf4cfa00d5fcafdb0b3170c9
roma <rbrian.0111@gmail.com>
2025-10-01 17:52:40 -0300

    agragación de 1 Clase y su config.

--- POO_1/1_Clases/Program.cs
+++ POO_1/1_Clases/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _1_Clases
 {
@@ -10,6 +7,96 @@ namespace _1_Clases
     {
         static void Main(string[] args)
         {
+            char opcion = 'n';
+            List<Persona> personas = new List<Persona>();
+
+            while (opcion == 'n')
+            {
+
+
+
+                //Pregunta para salir
+                Console.Write("¿Desea salir? (n/s): ");
+                opcion = Convert.ToChar(Console.ReadLine());
+
+            }
+
+            verLista(personas);
+
+
+        }
+
+        static void verLista(List<Persona> personas)
+        {
+            foreach (Persona persona in personas)
+            {
+                //Console.WriteLine(persona.);
+            }
         }
     }
+
+    //Variable Globlal
+    class Persona
+    {
+        //Propiedades
+        private string nombre;
+        private string apellido;
+        private int edad;
+        private bool adulto;
+
+        //Getters y Setters:
+        // Get y Set de nombre
+        public string Nombre
+        {
+            get
+            {
+                return nombre;
+            }
+            set
+            {
+                nombre = value;
+            }
+        }
+
+        // Get y Set de apellido
+        public string Apellido
+        {
+            get
+            {
+                return apellido;
+            }
+            set
+            {
+                nombre = value;
+            }
+        }
+
+        // Get y Set de edad
+        public int Edad
+        {
+            get
+            {
+                return edad;
+            }
+            set
+            {
+                if (value >= 18)
+                {
+                    edad = value;
+                    adulto = true;
+                }
+                else
+                {
+                    edad = value;
+                    adulto = false;
+                }
+            }
+
+        }
+
+
+    }
+
+
+
 }

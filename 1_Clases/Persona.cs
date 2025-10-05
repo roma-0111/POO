e03452fc6aae530bb7903e472a13b1db2bdd7375
roma <rbrian.0111@gmail.com>
2025-10-05 00:54:09 -0300

    Actualización general

---
+++ 1_Clases/Persona.cs
@@ -0,0 +1,49 @@
+﻿namespace _1_Clases
+{
+    internal class Persona
+    {
+        //Constructores
+        public Persona(string nombre)
+        {
+            Nombre = nombre;
+        }
+        public Persona() { }
+
+        //Propiedades
+        // Simplicado.
+        public string Nombre { get; set; }
+        private string DNI { get; set; }
+        // Privados.
+        private bool adultoSiONo = false;
+        private int edad;
+
+        //Setter y Getter
+        // semi-Simplificado
+        public int Edad
+        {
+            set { edad = value; }
+            get
+            {
+                //  Edad mayor o igual a 0
+                if (edad >= 18)
+                {
+                    adultoSiONo = true;
+                    return edad;
+                }
+                else
+                {
+                    adultoSiONo = false;
+                    return edad;
+                }
+            }
+        }
+        public bool GetADulto()
+        {
+            /*
+             * Agregar posible 2da restricción si cumple en un lugar 
+             * con distintas leyes...
+             */
+            return adultoSiONo;
+        }
+    }
+}
